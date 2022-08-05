17a638b57ff149a625e9cb11873eb0cce7bae5e3
unknown <you@example.comr.mahmud514@gmail.com>
2022-08-05 18:56:58 +0600

    handled dynamic file name

--- src/test/java/excelvalidation/readexcel.java
+++ src/test/java/excelvalidation/readexcel.java
@@ -4,10 +4,9 @@ package excelvalidation;
 
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
-import com.relevantcodes.extentreports.LogStatus;
-import excelvalidation.util.*;
+import excelvalidation.driver.DriverManager;
+import excelvalidation.util.automationReporter;
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.filefilter.TrueFileFilter;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
@@ -15,20 +14,47 @@ import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import java.io.*;
 import java.util.Iterator;
-import excelvalidation.driver.DriverManager;
 import excelvalidation.module.*;
+
 public class readexcel {
     public static ExtentReports extent;
     public static ExtentTest test;
 
 
     public static final String dirPath = new File("").getAbsolutePath();
-    public static final String EXCEL_FILE = dirPath +"\\Data\\Open_Invoice_List.xlsx";
+    public static final String dataPath = dirPath+"\\"+"Data";
+    //public static final String EXCEL_FILE = dirPath +"\\Data\\Open_Invoice_List.xlsx";
+
+
+    public static String getFile()
+    {
+         String dataFile = "";
+        //Creating a File object for directory
+        File directoryPath = new File(dataPath);
+        //Creating filter for jpg files
+        FilenameFilter xlsxFilefilter = new FilenameFilter(){
+            public boolean accept(File dir, String name) {
+                String lowercaseName = name.toLowerCase();
+                if (lowercaseName.endsWith(".xlsx")) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        };
+        String excelFilesList[] = directoryPath.list(xlsxFilefilter);
+        //System.out.println("List of the Excel files in the specified directory:");
+        for(String fileName : excelFilesList) {
+            dataFile = fileName;
+            break;
+        }
+        return "\\"+dataFile;
+    }
+
 
     private static int getLastFilledColumn(Sheet sheet) {
         int result = 0;
@@ -39,86 +65,88 @@ public class readexcel {
         return result;
     }
 
-    public static void readXLSFile() {
-
+    public static void readXLSFile(String fileName) {
         try {
-            InputStream ExcelFileToRead = new FileInputStream(EXCEL_FILE);
+            InputStream ExcelFileToRead = new FileInputStream(dataPath+fileName);
             XSSFWorkbook wb = new XSSFWorkbook(ExcelFileToRead);
             XSSFWorkbook workbook = null;
 
-            // sheet Iterator
-            for(int sheetNumber=0; sheetNumber<wb.getNumberOfSheets();sheetNumber++) {
-                XSSFSheet sheet = wb.getSheetAt(sheetNumber);
+
+            for(int k=0; k<wb.getNumberOfSheets();k++) {
+                XSSFSheet sheet = wb.getSheetAt(k);
                 Boolean isRowEmpty=false;
 
-//
-//                for(int i = 0; i <= sheet.getLastRowNum(); i++){
-//                    System.out.println(i + "   ---- " +sheet.getRow(i).getLastCellNum());
-//
-//                    if(sheet.getRow(i)==null){
-//                        sheet.shiftRows(i + 1, sheet.getLastRowNum(), -1);
-//                        i--;
-//                        continue;
+
+                for(int i = 0; i <= sheet.getLastRowNum(); i++){
+                    //     System.out.println(i + "   ---- " +sheet.getRow(i).getLastCellNum());
+
+                    if(sheet.getRow(i)==null){
+                        sheet.shiftRows(i + 1, sheet.getLastRowNum(), -1);
+                        i--;
+                        continue;
+                    }
+
+                    for(int j=0; j<sheet.getRow(i).getLastCellNum();j++){
+
+                        try {
+                            if(sheet.getRow(i).getCell(j).toString().trim().equals(""))
+                            {
+                                isRowEmpty=true;
+                            }else {
+                                isRowEmpty=false;
+                                break;
+                            }
+                        } catch(Exception e) {
+
+                        }
+
+                    }
+                    if(isRowEmpty==true){
+                        sheet.shiftRows(i+ 1, sheet.getLastRowNum(), -1);
+                        i--;
+                    }
+                }
+//                Iterator<Row> iter = sheet.rowIterator();
+//                int firstColumn = (iter.hasNext() ? Integer.MAX_VALUE : 0);
+//                int endColumn = 0;
+//                while (iter.hasNext()) {
+//                    Row row = iter.next();
+//                    short firstCell = row.getFirstCellNum();
+//                    if (firstCell >= 0) {
+//                        firstColumn = Math.min(firstColumn, firstCell);
+//                        endColumn = Math.max(endColumn, row.getLastCellNum());
 //                    }
+//                }
 //
-//                    for(int j=0; j<sheet.getRow(i).getLastCellNum();j++){
-//
-//                        if(sheet.getRow(i).getCell(j).toString().trim().equals(""))
-//                        {
-//                            isRowEmpty=true;
-//                        }else {
-//                            isRowEmpty=false;
-//                            break;
+//                // main logic block
+//                for (int i = 0; i< sheet.getLastRowNum(); i++) {
+//                    if (sheet.getRow(i) != null) {
+//                        isRowEmpty = true;
+//                        Row row = sheet.getRow(i);
+//                        for (int j = firstColumn; j < endColumn; j++) {
+//                            if (j >= row.getFirstCellNum() && j < row.getLastCellNum()) {
+//                                Cell cell = row.getCell(j);
+//                                if (cell != null) {
+//                                    if (!cell.getStringCellValue().equals("")) {
+//                                        isRowEmpty = false;
+//                                        break;
+//                                    }
+//                                }
+//                            }
+//                        }
+//                        //if empty
+//                        if (isRowEmpty) {
+//                           System.out.println("Found empty row on: " + row.getRowNum());
+//                            sheet.shiftRows(row.getRowNum() + 1, sheet.getLastRowNum(), -1);
+//                            i--;
 //                        }
 //                    }
-//                    if(isRowEmpty==true){
-//                        sheet.shiftRows(i+ 1, sheet.getLastRowNum(), -1);
+//                    // if row is null
+//                    else{
+//                        sheet.shiftRows(i + 1, sheet.getLastRowNum(), -1);
 //                        i--;
 //                    }
 //                }
-                Iterator<Row> iter = sheet.rowIterator();
-                int firstColumn = (iter.hasNext() ? Integer.MAX_VALUE : 0);
-                int endColumn = 0;
-                while (iter.hasNext()) {
-                    Row row = iter.next();
-                    short firstCell = row.getFirstCellNum();
-                    if (firstCell >= 0) {
-                        firstColumn = Math.min(firstColumn, firstCell);
-                        endColumn = Math.max(endColumn, row.getLastCellNum());
-                    }
-                }
-
-                // main logic block
-                //row iterator
-                for (int rowNumber = 0; rowNumber< sheet.getLastRowNum(); rowNumber++) {
-                    if (sheet.getRow(rowNumber) != null) {
-                        isRowEmpty = true;
-                        Row row = sheet.getRow(rowNumber);
-                        // column iterator
-                        for (int columnNumber = firstColumn; columnNumber < endColumn; columnNumber++) {
-                            if (columnNumber >= row.getFirstCellNum() && columnNumber < row.getLastCellNum()) {
-                                Cell cell = row.getCell(columnNumber);
-                                if (cell != null) {
-                                    if (!cell.getStringCellValue().equals("")) {
-                                        isRowEmpty = false;
-                                        break;
-                                    }
-                                }
-                            }
-                        }
-                        //if empty
-                        if (isRowEmpty) {
-                            System.out.println("Found empty row on: " + row.getRowNum());
-                            sheet.shiftRows(row.getRowNum() + 1, sheet.getLastRowNum(), -1);
-                            rowNumber--;
-                        }
-                    }
-                    // if row is null
-                    else{
-                        sheet.shiftRows(rowNumber + 1, sheet.getLastRowNum(), -1);
-                        rowNumber--;
-                    }
-                }
 
 
                 //System.out.println(sheet.getLastRowNum());
@@ -127,7 +155,7 @@ public class readexcel {
                 int lastFilledColumn = getLastFilledColumn(sheet);
                 //System.out.println(lastFilledColumn);
 
-                FileOutputStream fileOut = new FileOutputStream(dirPath +"\\Data\\Open_Invoice_List.xlsx");
+                FileOutputStream fileOut = new FileOutputStream(dataPath+fileName);
                 wb.write(fileOut);
                 fileOut.close();
 //                Iterator rows = sheet.rowIterator();
@@ -182,6 +210,7 @@ public class readexcel {
     public static void main(String[] args) throws Exception {
 //        File file = new File("C:\\Users\\DSi\\Downloads\\Data Bleanding File Two.xlsx");
 //        file.delete();
+
         //Reports
         automationReporter.initializeReporter();
         //create a new download directory if it doesn't exist already, skip if there is already one
@@ -189,15 +218,16 @@ public class readexcel {
         //Switch driver to project driver @parvez \\resolved
         WebDriver driver = DriverManager.getDriver(false); //flag is for switch between headless and headed
         //Flow should be created for AMTDirect @ Parvez
-        automationReporter.startReporter("Login Test");
+       automationReporter.startReporter("Login Test");
            loginModule.execute(driver,test);
         automationReporter.endReporter();
-//        automationReporter.startReporter("AP Module");
-//          apManagerModule.execute(driver,test);
-//        automationReporter.endReporter();
+        automationReporter.startReporter("AP Module");
+          apManagerModule.execute(driver,test);
+        automationReporter.endReporter();
         moveFiles(dirPath);
         automationReporter.closeReporter();
         System.out.println("Finished Testing");
         driver.close();
+        readXLSFile(getFile());
     }
 }
